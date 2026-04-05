6f28ee418a98dada5608dd5e68c7a25330d9d895
MaidenOfAir <supernovastarbright@gmail.com>
2026-04-05 04:19:34 -0400

    Added logic for gymnast.py. Next steps: Imports, push_untrip(), cleanup, bugfixing

--- src/magsim/racers/gymnast.py
+++ src/magsim/racers/gymnast.py
@@ -33,53 +33,11 @@ if TYPE_CHECKING:
     from magsim.engine.game_engine import GameEngine
 
 
-@dataclass
-class ShoeSprint(RacerModifier, RollModificationMixin):
-    name: AbilityName | ModifierName = "ShoeSprint"
-
-    @override
-    def modify_roll(
-        self,
-        query: MoveDistanceQuery,
-        owner_idx: int | None,
-        engine: GameEngine,
-        rolling_racer_idx: int,
-    ) -> list[AbilityTriggeredEventOrSkipped]:
-        if owner_idx is None:
-            msg = f"owner_idx should never be None for {self.name}"
-            raise ValueError(msg)
-
-        owner = engine.get_racer(owner_idx)
-
-        ActiveRacers = engine.get_active_racers()
-
-        racers_ahead = [r for r in ActiveRacers if r.position > owner.position]
-
-#         Skips ability if shoe is not behind any racers
-        if racers_ahead:
-
-            #Callout for shoe movement bonus
-            engine.log_info(f"{owner.repr} gets a movement bonus of {len(racers_ahead)} because there are {len(racers_ahead)} racers ahead of it")
-
-            # Bonus to main move
-            query.modifiers.append(len(racers_ahead))
-            query.modifier_sources.append((self.name, len(racers_ahead)))
-
-            return [
-                AbilityTriggeredEvent(
-                    owner_idx,
-                    self.name,
-                    phase=Phase.ROLL_WINDOW,
-                    target_racer_idx=owner_idx,
-                ),
-            ]
-        return []
-
 
 @dataclass
-class ShoeLaced(Ability, LifecycleManagedMixin):
-    name: AbilityName = "ShoeLaced"
-    triggers: tuple[type[GameEvent], ...] = (PostMoveEvent,PostWarpEvent,)
+class GymnastCartwheel(Ability):
+    name: AbilityName = "GymnastCartwheel"
+    triggers: tuple[type[GameEvent], ...] = (TripCmdEvent, TripRecoveryEvent, RollResultEvent,)
 
     @override
     def execute(
@@ -88,62 +46,81 @@ class ShoeLaced(Ability, LifecycleManagedMixin):
         owner: ActiveRacerState,
         engine: GameEngine,
         agent: Agent,
-    ):
-        if not isinstance(event, PostMoveEvent or PostWarpEvent):
+    ) -> AbilitytriggeredEventOrSkipped:
+
+        if not isinstance(event, TripCmdEvent or TripRecoveryEvent or RollResultEvent):
             return "skip_trigger"
 
-        # CASE 1: Shoe moved onto someone
-        if event.target_racer_idx == owner.idx:
-            racers_on_space = engine.get_racers_at_position(
-                tile_idx=owner.position,
-                except_racer_idx=owner.idx,
+
+#         When Gymnast stands up...
+        if isinstance(event, TripRecoveryEvent) and event.target_racer_idx == owner_idx:
+
+
+#             ...announce it and move forward 1
+            engine.log_info(
+                f"{owner.repr} finishes their {self.name} cartwheel!"
+            )
+            push_move(
+                engine,
+                distance=1,
+                phase=event.phase,
+                moved_racer_idx=owner.idx,
+                source=self.name,
+                responsible_racer_idx=owner.idx,
+                emit_ability_triggered="after_resolution",
             )
 
-            if trippers := [r for r in racers_on_space]:
-                engine.log_info(
-                    f"{owner.repr} moved onto {owner.position} alongside {', '.join([t.repr for t in trippers])} and trips itself with {self.name}!",
-                )
-                push_trip(
+
+#         When gymnast gets tripped...
+        if isinstance(event, TripCmdEvent) and event.target_racer_idx == owner_idx:
+
+#             Untrip them if they are tripped
+            if owner.tripped:
+                push_untrip(
                     engine,
                     phase=event.phase,
-                    tripped_racer_idx=owner.idx,
+                    untripped_racer_idx=owner_idx,
                     source=self.name,
                     responsible_racer_idx=owner.idx,
                     emit_ability_triggered="after_resolution",
                 )
 
-        # CASE 2: Someone else moved onto shoe
-        # else:
-        #     mover = engine.get_racer(event.target_racer_idx)
-        #     if mover.active and mover.position == owner.position:
-        #         if not owner.tripped:
-        #             # only log when actually tripping
-        #             engine.log_info(
-        #                 f"{mover.repr} stepped onto {owner.repr} and trips up {owner.repr} due to {self.name}!",
-        #             )
-        #         push_trip(
-        #             engine,
-        #             phase=event.phase,
-        #             tripped_racer_idx=owner.idx,
-        #             source=self.name,
-        #             responsible_racer_idx=owner.idx,
-        #             emit_ability_triggered="after_resolution",
-        #         )
+#             Otherwise announce and move forward 1
+            engine.log_info(
+                f"{owner.repr} started a cartwheel due to {self.name}!"
+            )
+            push_move(
+                engine,
+                distance=1,
+                phase=event.phase,
+                moved_racer_idx=owner.idx,
+                source=self.name,
+                responsible_racer_idx=owner.idx,
+                emit_ability_triggered="after_resolution",
+            )
+
+#         When Someone rolls...
+        if isinstance(RollResultEvent):
+
+#             Check if it is 3 or 4
+            if event.dice_value == 3 or 4:
+
+#                 If so, announce it
+                engine.log_info(
+                    f"{engine.get_racer(event.target_racer_idx).repr} rolled a {event.dice_value}, so {owner.repr} uses {self.name}",
+                )
+
+#                 and immediately push a trip for gymnast
+                push_trip(
+                    engine,
+                    phase=event.phase,
+                    tripped_racer_idx=owner_idx,
+                    source=self.name,
+                    responsible_racer_idx=owner.idx,
+                    emit_ability_triggered="after_resolution",
+                )
+
 
         return "skip_trigger"
 
-    @override
-    def on_gain(self, engine: GameEngine, owner_idx: int):
-        add_racer_modifier(
-            engine,
-            owner_idx,
-            ShoeSprint(owner_idx=owner_idx),
-        )
 
-    @override
-    def on_loss(self, engine: GameEngine, owner_idx: int):
-        remove_racer_modifier(
-            engine,
-            owner_idx,
-            ShoeSprint(owner_idx=owner_idx),
-        )
